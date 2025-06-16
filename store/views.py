26abb3e2a0df5bfd530a630167e8fda43afb6351
phitron-demo <sojibhasan5800@gmail.com>
2025-06-16 08:52:25 +0600

    pagination bar add of sotre page

--- store/views.py
+++ store/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
 from category.models import Category
-# from carts.models import CartItem
+from carts.models import CartItem
 # from django.db.models import Q
 
-# from carts.views import _cart_id
+from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # from django.http import HttpResponse
 # from .forms import ReviewForm
@@ -19,7 +19,7 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
-        paginator = Paginator(products, 1)
+        paginator = Paginator(products, 2)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
@@ -40,6 +40,7 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id = _cart_id(request),product=single_product).exists()
         
     except Exception as e:
         raise e
@@ -60,7 +61,7 @@ def product_detail(request, category_slug, product_slug):
 
     context = {
         'single_product': single_product,
-        # 'in_cart'       : in_cart,
+        'in_cart'       : in_cart,
         # 'orderproduct': orderproduct,
         # 'reviews': reviews,
         # 'product_gallery': product_gallery,
