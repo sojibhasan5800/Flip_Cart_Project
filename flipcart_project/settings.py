4ae07fdbfe0c0a3d576ff5d3ec7bf1b47962cafb
phitron-demo <sojibhasan5800@gmail.com>
2025-06-29 08:04:21 +0600

    project ready deploy every problem fix vercel to render

--- flipcart_project/settings.py
+++ flipcart_project/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
+# ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
