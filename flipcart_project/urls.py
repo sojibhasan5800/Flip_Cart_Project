d739c714d9da3cab74a51813a9d03b1930809c2b
phitron-demo <sojibhasan5800@gmail.com>
2025-10-04 11:30:01 +0600

    feat: create dashboard app for seller analytics

--- flipcart_project/urls.py
+++ flipcart_project/urls.py
@@ -51,6 +51,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('orders/', include('orders.urls')),
     path('category/', include('category.urls')),
+    path('seller_dashboard/', include('seller_dashboard.urls')),
     # # path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     # # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
