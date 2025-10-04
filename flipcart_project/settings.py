0346983f6e54e5fbb479fe44f56eea6b9f2ad7f8
phitron-demo <sojibhasan5800@gmail.com>
2025-10-04 11:33:48 +0600

    add seller dashboard view and AJAX endpoint for live analytics

--- flipcart_project/settings.py
+++ flipcart_project/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'store',
     'carts',
     'orders',
+    'seller_dashboard',
 ]
 
 # -----------------------------
--- seller_dashboard/views.py
+++ seller_dashboard/views.py
@@ -1,3 +1,25 @@
+# seller_dashboard/views.py
 from django.shortcuts import render
+from orders.models import OrderProduct
+from .models import SellerAnalytics
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
-# Create your views here.
+@login_required
+def dashboard(request):
+    seller = request.user
+    analytics = SellerAnalytics.objects.filter(seller=seller)
+
+    context = {
+        'analytics': analytics
+    }
+    return render(request, 'seller_dashboard/dashboard.html', context)
+
+# AJAX endpoint for live update
+@login_required
+def dashboard_ajax(request):
+    seller = request.user
+    analytics = SellerAnalytics.objects.filter(seller=seller).values(
+        'product__product_name','total_orders','total_revenue'
+    )
+    return JsonResponse(list(analytics), safe=False)
