7e19512375b952a45c394fcf6709d6024d217ffe
sojibhasan580 <sojibhasan5800@gmail.com>
2025-10-05 15:21:36 +0600

    Update SellerAnalytics model to store cached analytics

--- flipcart_project/asgi.py
+++ flipcart_project/asgi.py
@@ -1,23 +1,16 @@
-"""
-ASGI config for flipcart_project project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import seller_dashboard.routing
+import django
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flipcart_project.settings')
+django.setup()
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import seller_dashboard.routing  # Point
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": get_default_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
             seller_dashboard.routing.websocket_urlpatterns
