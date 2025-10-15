511c4e4f4e37b365f2e673b715632519a74312cd
phitron-demo <sojibhasan5800@gmail.com>
2025-10-15 09:45:55 +0600

    add DRF serializers for Cart and CartItem models

---
+++ carts/serializers.py
@@ -0,0 +1,23 @@
+# carts/api/serializers.py
+from rest_framework import serializers
+from .models import Cart, CartItem
+from store.serializers import ProductSerializer, VariationSerializer
+from accounts.serializers import AccountSerializer
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = ['id', 'cart_id', 'date_added']
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    variations = VariationSerializer(many=True, read_only=True)
+    user = AccountSerializer(read_only=True)
+    sub_total = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'user', 'product', 'variations', 'cart', 'quantity', 'is_active', 'sub_total']
+
+    def get_sub_total(self, obj):
+        return obj.sub_total()
