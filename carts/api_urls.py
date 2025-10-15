9559eeb163eccffcd408fa283cc6af5d459a8923
phitron-demo <sojibhasan5800@gmail.com>
2025-10-15 09:46:54 +0600

    add DRF API urls and namespace carts_api

---
+++ carts/api_urls.py
@@ -0,0 +1,10 @@
+# carts/api/urls.py
+from django.urls import path
+from .views import CartItemListAPIView, CartItemDetailAPIView
+
+app_name = "carts_api"
+
+urlpatterns = [
+    path('', CartItemListAPIView.as_view(), name='cart_items'),
+    path('<int:pk>/', CartItemDetailAPIView.as_view(), name='cart_item_detail'),
+]
