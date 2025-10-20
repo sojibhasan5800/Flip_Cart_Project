666b779fbb31b075f607ec2be15bbaa914566edb
phitron-demo <sojibhasan5800@gmail.com>
2025-10-20 08:14:43 +0600

    feat(orders): Update DRF API urls for Order, OrderProduct, Payment

--- orders/api_urls.py
+++ orders/api_urls.py
@@ -1,11 +1,17 @@
-# orders/api/urls.py
-from django.urls import path
-from .api_views import OrderListCreateAPIView, OrderDetailAPIView, PaymentSuccessAPIView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import api_views
 
-app_name = "orders_api"
+app_name = 'orders_api'
+
+router = DefaultRouter()
+router.register('orders', api_views.OrderViewSet, basename='orders')
+router.register('payments', api_views.PaymentViewSet, basename='payments')
+router.register('order-products', api_views.OrderProductViewSet, basename='order-products')
 
 urlpatterns = [
-    path('', OrderListCreateAPIView.as_view(), name='list_create_order'),
-    path('<int:pk>/', OrderDetailAPIView.as_view(), name='detail_order'),
-    path('payment-success/', PaymentSuccessAPIView.as_view(), name='payment_success_api'),
+    path('', include(router.urls)),
+    path('stripe/webhook/', api_views.StripeWebhookAPIView.as_view(), name='stripe-webhook'),
+    path('stripe/success/', api_views.StripeSuccessAPIView.as_view(), name='stripe-success'),
+    path('stripe/cancel/', api_views.StripeCancelAPIView.as_view(), name='stripe-cancel'),
 ]
